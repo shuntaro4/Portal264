fe3d795d4a5dec24ee783c5d64c6baeb3cd9668b
shuntaro4 <shuntaro4@gmail.com>
2018-02-08 23:27:55 +0900

    test: add admin model test.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -6,7 +6,8 @@ class Admin < ActiveRecord::Base
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence:   true,
                     format:     { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+                    uniqueness: { case_sensitive: false },
+                    length:     { maximum: 50 }
     has_secure_password
     validates :password, length: { minimum: 6 }
 
---
+++ spec/factories/admins.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :admin do
+    name "管理者"
+    email "admin@test.com"
+    password "12345678"
+  end
+end
