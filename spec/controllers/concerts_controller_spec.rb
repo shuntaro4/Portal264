f4d8eb58216ba2f56690c2447360934199d2ce17
shuntaro4 <shuntaro4@gmail.com>
2018-02-12 14:52:54 +0900

    test: add concerts GET#new test.

--- spec/controllers/concerts_controller_spec.rb
+++ spec/controllers/concerts_controller_spec.rb
@@ -2,56 +2,89 @@ require 'rails_helper'
 
 RSpec.describe ConcertsController, type: :controller do
   let(:concert) { FactoryBot.create(:concert) }
+  let(:admin) { FactoryBot.create(:admin) }
+  delegate :sign_in, to: 'controller.view_context'
 
   describe "GET index" do
-    it "has a 200 status code." do
+    before do
       get :index
+    end
+    it "has a 200 status code." do
       expect(response.status).to eq(200)
     end
     it "assigns @title." do
-      get :index
       expect(assigns(:title)).to eq("コンサート一覧")
     end
     it "assigns @concerts." do
-      concert = FactoryBot.create(:concert)
-      get :index
       expect(assigns(:concerts)).to eq([concert])
     end
     it "reders the index template." do
-      get :index
       expect(response).to render_template("index")
     end
   end
 
   describe "GET show" do
     context "when concert id is exists" do
-      it "has a 200 status code." do
+      before do
         get :show, id: concert.id
+      end
+      it "has a 200 status code." do
         expect(response.status).to eq(200)
       end
       it "assigns @title." do
-        get :show, id: concert.id
         expect(assigns(:title)).to eq("コンサート詳細")
       end
       it "assigns @concert." do
-        get :show, id: concert.id
         expect(assigns(:concert)).to eq(concert)
       end
       it "reders the show template." do
-        get :show, id: concert.id
         expect(response).to render_template(:show)
       end
     end
 
     context "when concert id is not exists" do
-      it "redirect the index template." do
+      before do
         get :show, id: 100
+      end
+      it "redirect the index template." do
         expect(response).to redirect_to(concerts_index_path)
       end
       it "has a flash[:danger]." do
-        get :show, id: 100
         expect(flash[:danger]).to eq("コンサート情報が存在しません。")
       end
     end
+  end
+
+  describe "GET new" do
+    context "when you are signed in" do
+      before do
+        sign_in admin
+        get :new
+      end
+      it "has a 200 status code." do
+        expect(response.status).to eq(200)
+      end
+      it "assigns @title." do
+        expect(assigns(:title)).to eq("コンサート新規登録")
+      end
+      it "assigns @url." do
+        expect(assigns(:url)).to eq(concerts_create_path)
+      end
+      it "assigns @concert." do
+        expect(assigns(:concert)).to be_a_new(Concert)
+      end
+      it "reders the new template." do
+        expect(response).to render_template(:new)
+      end
+    end
+
+    context "when you are not signed in" do
+      before do
+        get :new
+      end
+      it "has a 302 status code." do
+        expect(response.status).to eq(302)
+      end
+    end  
   end
 end
