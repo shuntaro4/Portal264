1cb7b222e29bc2b3eef6601c43a16e136b05a621
shuntaro4 <shuntaro4@gmail.com>
2018-02-28 23:14:29 +0900

    fix: to delay evalutaion of concert model in reservation factory.

--- spec/factories/reservations.rb
+++ spec/factories/reservations.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :reservation do
-    concert FactoryBot.build(:concert)
+    concert { FactoryBot.build(:concert) }
     name "John Titor"
     mail "john-titor@testmail.com"
     ticket 3
