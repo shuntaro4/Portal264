2df567ea25aca54ed335a28dc09303e81736d615
shuntaro4 <shuntaro4@gmail.com>
2018-03-17 18:53:10 +0900

    fix: fix flash type from 'error' to 'danger'.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,11 @@ class SessionsController < ApplicationController
     def create
         user = Admin.find_by(email: params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
+            cookies[:remember_tokena] = 'ok'
             sign_in user
             redirect_to root_path
         else
-            flash.now[:error] = "Invalid email/password combination"
+            flash.now[:danger] = "Invalid email/password combination"
             render "new"
         end
     end
