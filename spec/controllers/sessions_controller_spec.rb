6cbf3615e3248353bf878d44549856796ea57388
shuntaro4 <shuntaro4@gmail.com>
2018-03-17 18:53:45 +0900

    test: add sessions POST#create test.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  let(:admin) { FactoryBot.create(:admin) }
+
   describe "GET new" do
     before do
       get :new
@@ -10,6 +12,57 @@ RSpec.describe SessionsController, type: :controller do
     end
     it "reders the new template." do
       expect(response).to render_template("new")
+    end
+  end
+
+  describe "POST create" do
+    context "when admin user is exists" do
+      context "when the authentication succeeds" do
+        before do
+          post :create, session: {
+            email: admin.email,
+            password: admin.password
+          }
+        end
+        it "the remember token save in cookies." do
+          cookies_remenber_token = Admin.encrypt(response.cookies["remember_token"])
+          expect(Admin.find_by(remember_token: cookies_remenber_token)).to eq(admin)
+        end
+        it "assigns current_user." do
+          expect(assigns(:current_user)).to eq(admin)
+        end
+        it "redirect root path." do
+          expect(response).to redirect_to(root_path)
+        end
+      end
+      context "when the authentication fails" do
+        before do
+          post :create, session: {
+            email: admin.email,
+            password: 999999999
+          }
+        end
+        it "has a flash[:danger]." do
+          expect(flash[:danger]).to eq("Invalid email/password combination")
+        end
+        it "render the new template." do
+          expect(response).to render_template(:new)
+        end
+      end
+    end
+    context "when admin user is not exists" do
+      before do
+        post :create, session: {
+          email: "test@admin.com",
+          password: 999999999
+        }
+      end
+      it "has a flash[:danger]." do
+        expect(flash[:danger]).to eq("Invalid email/password combination")
+      end
+      it "render the new template." do
+        expect(response).to render_template(:new)
+      end
     end
   end
 end
