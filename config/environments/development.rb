cd554b5f29ba29176ab55ac4cc515af60b122ac3
shuntaro4 <shuntaro4@gmail.com>
2018-04-12 22:34:35 +0900

    fix: run `rails app:update`.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,6 +48,10 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  # Use an evented file watcher to asynchronously detect changes in source code,
+  # routes, locales, etc. This feature depends on the listen gem.
+  config.file_watcher = ActiveSupport::FileUpdateChecker
+
   # mail 設定
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,14 +73,22 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-  # Disable automatic flushing of the log to improve performance.
-  # config.autoflush_log = false
-
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  # Use a different logger for distributed setups.
+  # require 'syslog/logger'
+  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  end
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
 
   # mail 設定
   config.action_mailer.delivery_method = :smtp
